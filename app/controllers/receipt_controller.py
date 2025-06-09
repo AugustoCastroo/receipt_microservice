f6405059ea2fbc84944291cd86ca7c74c7c83a14
jose-ruti <j.ruti@alumno.um.edu.ar>
2025-06-09 17:11:38 -0300

    Add receipt controller and schema implementation; fix image name in docker-compose

---
+++ app/controllers/receipt_controller.py
@@ -0,0 +1,57 @@
+import logging
+from flask import Blueprint, request
+from app.services import ReceiptService
+from app.mapping import ReceiptSchema
+from app.mapping import MessageMap
+from app.services import MessageBuilder
+
+receipt_bp = Blueprint('receipt', __name__)
+
+@receipt_bp.route('/receipt/<int:id>', methods=['GET'])
+def get(id: int):
+    receipt = ReceiptService.find(id)
+    receipt_schema = ReceiptSchema()
+    receipt_data = receipt_schema.dump(receipt)
+    message_builder = MessageBuilder()
+    message_finish = message_builder.add_message('Se encontró el recibo').add_data({'receipt': receipt_data}).build()
+    message_map = MessageMap()
+    return message_map.dump(message_finish), 200
+
+@receipt_bp.route('/receipts', methods=['GET'])
+def get_all():
+    receipts = ReceiptService.find_all()
+    receipt_schema = ReceiptSchema()
+    receipts_data = receipt_schema.dump(receipts, many=True)
+    message_builder = MessageBuilder()
+    message_finish = message_builder.add_message('Se encontraron todos los recibos').add_data({'receipts': receipts_data}).build()
+    message_map = MessageMap()
+    return message_map.dump(message_finish), 200
+
+@receipt_bp.route('/receipts', methods=['POST'])
+def post():
+    receipt_schema = ReceiptSchema()
+    receipt = receipt_schema.load(request.json)
+    ReceiptService.save(receipt)
+    message_map = MessageMap()
+    message_builder = MessageBuilder()
+    message_finish = message_builder.add_message('Recibo creado').build()
+    return message_map.dump(message_finish), 200
+
+@receipt_bp.route('/receipts/<int:id>', methods=['PUT'])
+def put(id: int):
+    receipt_schema = ReceiptSchema()
+    new_receipt = receipt_schema.load(request.json)
+    ReceiptService.update(new_receipt)
+    message_map = MessageMap()
+    message_builder = MessageBuilder()
+    message_finish = message_builder.add_message('Recibo actualizado').build()
+    return message_map.dump(message_finish), 200
+
+@receipt_bp.route('/receipts/<int:id>', methods=['DELETE'])
+def delete(id: int):
+    receipt = ReceiptService.find(id)
+    ReceiptService.delete(receipt)
+    message_map = MessageMap()
+    message_builder = MessageBuilder()
+    message_finish = message_builder.add_message(f'Se eliminó el recibo {id}').build()
+    return message_map.dump(message_finish), 200
---
+++ app/mapping/receipt_schema.py
@@ -0,0 +1,12 @@
+from marshmallow import Schema, fields, post_load
+from app.models.receipt import Receipt
+
+class ReceiptSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    id_header = fields.Integer(required=True)
+    id_footer = fields.Integer(required=True)
+    id_receipt_type = fields.Integer(required=True)
+
+    @post_load
+    def make_receipt(self, data, **kwargs):
+        return Receipt(**data)
